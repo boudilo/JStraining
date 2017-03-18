387ff08b8009d235584f6cbf98bbbe64d6797531
Артём Логинов < >
2017-03-18 15:09:20 +0300

    Animation

--- app/js/myscript.js
+++ app/js/myscript.js
@@ -1,32 +1,8 @@
-// Callback
-function stringbuilder() {
-	var string = "";
-	for (var i = 0; i < arguments.length; i++) {
-		string = string + arguments[i] + " ";
-	}
-
-	return (function (replaser) {
-		while (string.indexOf(" ") != -1) {
-			string = string.trim().replace(" ", replaser);
-		}
-		return string;
-	})("!!!")
-}
-
-var fn1 = stringbuilder("sanya", "artik", "maks", "vadik", "dimasik");
-console.log(fn1);
-
-// RegExp
-var potentialNumber = "+375(29)0-0-0";
-var exp = /^\+\d+ ?\(\d+\) ?\d+[ -]?\d+[ -]?\d+/;
-var result = exp.test(potentialNumber);
-console.log(result);
-
 // PhoneForm
 function sendForm(e){
 
 	// получаем значение поля key
-	var keyBox = document.search.key;
+	var keyBox = document.phone.key;
 	var exp = /^\+\d+ ?\(\d+\) ?\d+[ -]?\d+[ -]?\d+/;
 	var val = keyBox.value;
 	if(exp.test(val)){
@@ -37,52 +13,27 @@ function sendForm(e){
 		e.preventDefault();
 }
 
-var sendButton = document.search.send;
+var sendButton = document.phone.send;
 sendButton.addEventListener("click", sendForm);
 
 // SelectForm
-var sensor = sensorsSelection.sensor.options[1];
-var sensorParams = {
-	index: sensor.index,
-	text: sensor.text,
-	value: sensor.value,
+var sensorsSelect = sensorsSelection.sensor;
+ 
+function changeOption(){
+     
+    var selection = document.getElementById("sensors");
+    var selectedOption = sensorsSelect.options[sensorsSelect.selectedIndex];
+    console.log("Вы выбрали: " + selectedOption.text);
 }
-sensor = sensorsSelection.parentNode;
-for (var key in sensorParams) {
-	var writer = document.createElement("p");
-	writer.textContent = sensorParams[key];
-	sensor.appendChild(writer);
-}
+ 
+sensorsSelect.addEventListener("change", changeOption);
 
-console.log(sensor.nodeName);
-
-// requestAnimationFrame
-/*var square = document.getElementById("rect");
+var square = document.getElementById("cone");
 var angle = 0;
 function rotate() {
-	angle = (angle + 1)%360;
-	square.style.transform = "rotate(" + angle + "deg)";
-	window.requestAnimationFrame(rotate);
-}
-var id = window.requestAnimationFrame(rotate);
-console.log(id);*/
-
-/*var start = null;
-var element = document.getElementById('rect');
-element.style.position = 'absolute';
-var angle = 0;
-
-function step(timestamp) {
-	if (!start) start = timestamp;
-	var progress = timestamp - start;
-	element.style.left = Math.min(progress / 10, 1000) + 'px';
-
-	angle = (angle + 3) % 360;
-	element.style.transform = "rotate(" + angle + "deg)";
-
-	if (progress < 10000) {
-		window.requestAnimationFrame(step);
-	}
+    angle = (angle + 2)%360;
+    square.style.transform = "rotate(" + angle + "deg)";
+    window.requestAnimationFrame(rotate);
 }
 
-window.requestAnimationFrame(step);*/
+setTimeout(rotate,10000);
